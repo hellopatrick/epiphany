d3838e4e1ff8c4b86a22cce21f422615592b36bc
Patrick Rogers <patrick@donotforget.xyz>
2020-12-05 21:06:12 -0800

    feat: bench

--- benches/aoc.rs
+++ benches/aoc.rs
@@ -7,14 +7,26 @@ pub fn day03_benchmark(c: &mut Criterion) {
     .map(|line| line.chars().map(|c| c == '#').collect())
     .collect();
 
-  c.bench_function("part 1", |b| {
+  c.bench_function("day04:01", |b| {
     b.iter(|| day03::first_star(black_box(&input)))
   });
 
-  c.bench_function("part 2", |b| {
+  c.bench_function("day04:02", |b| {
     b.iter(|| day03::second_star(black_box(&input)))
   });
 }
 
-criterion_group!(benches, day03_benchmark);
+pub fn day05_benchmark(c: &mut Criterion) {
+  let input: Vec<_> = include_str!("../data/05.txt").lines().collect();
+
+  c.bench_function("day05:01", |b| {
+    b.iter(|| day05::first_star(black_box(&input)))
+  });
+
+  c.bench_function("day05:02", |b| {
+    b.iter(|| day05::second_star(black_box(&input)))
+  });
+}
+
+criterion_group!(benches, day03_benchmark, day05_benchmark);
 criterion_main!(benches);
